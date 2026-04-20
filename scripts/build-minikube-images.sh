45dbafff6297188d3c951ed214ece6109db73ecb
vgmunoz <victor.gomez@nextdigital.es>
2026-04-20 19:15:43 +0200

    set final version 1.0.0

--- scripts/build-minikube-images.sh
+++ scripts/build-minikube-images.sh
@@ -7,9 +7,14 @@ REPO_ROOT="$(cd "${SCRIPT_DIR}/.." && pwd)"
 K8S_DIR="${REPO_ROOT}/poke-app/k8s"
 NAMESPACE="poke-app"
 HOSTNAME_VALUE="${HOSTNAME_VALUE:-poke.local}"
+IMAGE_TAG="${1:-}"
 
 log() {
   printf '\n==> %s\n' "$1"
+}
+
+usage() {
+  echo "Uso: $0 <image-tag>" >&2
 }
 
 require_command() {
@@ -19,16 +24,30 @@ require_command() {
   fi
 }
 
+update_deployment_image() {
+  local file="$1"
+  local image_name="$2"
+
+  perl -0pi -e "s|image: ${image_name}:[^\\s]+|image: ${image_name}:${IMAGE_TAG}|g" "${file}"
+}
+
+update_deployments() {
+  log "Actualizando deployment yamls con la version ${IMAGE_TAG}"
+  update_deployment_image "${K8S_DIR}/auth/deployment.yaml" "poke-auth"
+  update_deployment_image "${K8S_DIR}/back/deployment.yaml" "poke-back"
+  update_deployment_image "${K8S_DIR}/front/deployment.yaml" "poke-front"
+}
+
 build_images() {
-  log "Construyendo imagen poke-auth:latest"
-  docker build -t poke-auth:latest "${REPO_ROOT}/poke-app/auth"
+  log "Construyendo imagen poke-auth:${IMAGE_TAG}"
+  docker build -t "poke-auth:${IMAGE_TAG}" "${REPO_ROOT}/poke-app/auth"
 
-  log "Construyendo imagen poke-back:latest"
-  docker build -t poke-back:latest "${REPO_ROOT}/poke-app/back"
+  log "Construyendo imagen poke-back:${IMAGE_TAG}"
+  docker build -t "poke-back:${IMAGE_TAG}" "${REPO_ROOT}/poke-app/back"
 
-  log "Construyendo imagen poke-front:latest"
+  log "Construyendo imagen poke-front:${IMAGE_TAG}"
   docker build \
-    -t poke-front:latest \
+    -t "poke-front:${IMAGE_TAG}" \
     "${REPO_ROOT}/poke-app/front" \
     --build-arg VITE_API_BASE_URL=/api/v2 \
     --build-arg VITE_AUTH_BASE_URL=/auth
@@ -38,6 +57,12 @@ main() {
   require_command minikube
   require_command kubectl
   require_command docker
+  require_command perl
+
+  if [[ -z "${IMAGE_TAG}" ]]; then
+    usage
+    exit 1
+  fi
 
   log "Arrancando Minikube"
   minikube start
@@ -48,6 +73,7 @@ main() {
   log "Conectando al daemon Docker de Minikube"
   eval "$(minikube -p minikube docker-env --shell bash)"
 
+  update_deployments
   build_images
 }
 
